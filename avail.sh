6bb599256adf3ee849f4ec58a268421b0d150cf8
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-04-05 02:17:54 +0300

    update

--- avail.sh
+++ avail.sh
@@ -39,6 +39,43 @@ rm avail-light-linux-amd64.tar.gz
 sleep 1
 sudo mv avail-light-linux-amd64 /usr/local/bin/avail-light
 sudo chmod +x /usr/local/bin/avail-light
+#cretae sonfig
+if [ ! -d "$HOME/.avail" ]; then
+    mkdir $HOME/.avail
+fi
+if [ ! -d "$HOME/.avail/config" ]; then
+    mkdir $HOME/.avail/config
+fi
+if [ ! -d "$HOME/.avail/identity" ]; then
+    mkdir $HOME/.avail/identity
+fi
+if [ ! -d "$HOME/.avail/data" ]; then
+    mkdir $HOME/.avail/data
+fi
+if [ -z "$identity" ]; then
+    IDENTITY=$HOME/.avail/identity/identity.toml
+    if [ -f "$IDENTITY" ]; then
+        echo "🔑 Identity found at $IDENTITY."
+    else
+        echo "🤷 No identity set. This will be automatically generated at startup."
+    fi
+else
+    IDENTITY="$identity"
+fi
+CONFIG_PARAMS="bootstraps=['/dns/bootnode.1.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWBkLsNGaD3SpMaRWtAmWVuiZg1afdNSPbtJ8M8r9ArGRT','/dns/bootnode.2.lightclient.goldberg.avail.tools/tcp/37000/p2p/12D3KooWRCgfvaLSnQfkwGehrhSNpY7i5RenWKL2ARst6ZqgdZZd']\nfull_node_ws=['wss://rpc-goldberg.sandbox.avail.tools:443','wss://goldberg-rpc.fra.avail.tools:443']\nconfidence=99.0\navail_path='$HOME/.avail/data'\nkad_record_ttl=43200\not_collector_endpoint='http://otelcol.lightclient.goldberg.avail.tools:4317'\ngenesis_hash='6f09966420b2608d1947ccfb0f2a362450d1fc7fd902c29b67c906eaa965a7ae'\n"
+if [ -z "$config" ]; then
+        CONFIG="$HOME/.avail/config/config.yml"
+        if [ -f "$CONFIG" ]; then
+            echo "🗑️  Wiping old config file at $CONFIG."
+            rm $CONFIG
+        else
+            echo "🤷 No configuration file set. This will be automatically generated at startup."
+        fi
+        touch $CONFIG
+        echo -e $CONFIG_PARAMS >>$CONFIG
+    else
+        CONFIG="$config"
+    fi
 #create service node
     echo "[Unit]
 Description=Avail Light Client
@@ -46,7 +83,7 @@ After=network.target
 StartLimitIntervalSec=0
 [Service] 
 User=root 
-ExecStart=avail-light --network goldberg
+ExecStart=avail-light --network goldberg --config $CONFIG --identity $IDENTITY
 Restart=always 
 RestartSec=120
 [Install] 
