341dff4a104d86951c1fd901f0496dd548c5cbb6
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-04-05 17:32:53 +0300

    update

--- avail_docker.sh
+++ avail_docker.sh
@@ -30,8 +30,7 @@ read -p "FullNode Name : " NAME
 sudo apt update &> /dev/null
 apt-get install protobuf-compiler -y
 apt-get update && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y --no-install-recommends tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+. <(wget -qO- https://raw.githubusercontent.com/mgpwnz/VS/main/docker.sh)
 sleep 3
 sudo apt install wget -y &> /dev/null
 sudo apt-get install libgomp1 -y &> /dev/null
@@ -41,7 +40,7 @@ sudo mkdir $HOME/avail
 #download 
 cd $HOME/avail
 sudo docker run -v $(pwd)/state:/da/state:rw -v $(pwd)/keystore:/da/keystore:rw -e DA_CHAIN=goldberg -e DA_NAME=$NAME -p 37333:30333 -p 10615:9615 -p 10944:9944 -d --name=avail --restart unless-stopped availj/avail:v1.8.0.0 --rpc-cors=all --rpc-external --rpc-methods=unsafe --rpc-port 9944
-
+cd $HOME
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
